79a9ffb2e0c7d3df6b31d831d7735c66660124eb
Joanna Huang <joanna.huitzu.huang@gmail.com>
2013-06-05 16:12:29 +1000

    changed time.py on upstream again.

--- time.py
+++ time.py
@@ -5,3 +5,4 @@ print time.time()
 print "I've made some changes on upstream."
 print "people are working on upstream."
 print "I'm making my own changes in fork."
+print "I created some changes on upstream again."
