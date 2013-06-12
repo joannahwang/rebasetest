03425395fed97f1fe6ed0bd5a28e4b17deaf270c
Joanna Huang <joanna.huitzu.huang@gmail.com>
2013-06-12 20:02:43 +1000

    fork changed first line and added one line.

--- time.py
+++ time.py
@@ -2,7 +2,8 @@
 import time
 print time.time()
 
-print "I made some changes on upstream."
+print "I made some changes on fork."
+print "fork added one line"
 print "upstream added one line."
 print "upstream added another line."
 print "upstream added third line."
