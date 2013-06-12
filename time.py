53db7d666644cc719a25190bacd7cf1a8110248c
Joanna Huang <joanna.huitzu.huang@gmail.com>
2013-06-12 19:34:27 +1000

    upstream updated one line and added fourth line.

--- time.py
+++ time.py
@@ -5,4 +5,5 @@ print time.time()
 print "I made some changes on upstream."
 print "upstream added one line."
 print "upstream added another line."
-print "upstream added the third line."
+print "upstream added third line."
+print "upstream added four line."
