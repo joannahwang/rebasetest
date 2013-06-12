1cb876709c3ab45aa45d04fdd9b8dec7cbd08ad4
Joanna Huang <joanna.huitzu.huang@gmail.com>
2013-06-12 19:30:13 +1000

    collaborator added another line.

--- time.py
+++ time.py
@@ -4,6 +4,7 @@ print time.time()
 
 print "I made some changes on upstream."
 print "collaborator added one line."
+print "collaborator added another line."
 print "upstream added one line."
 print "upstream added another line."
 print "upstream added the third line."
