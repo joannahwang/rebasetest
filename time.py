3bb0e2f02c9b1b34b4db811a7d9acf6c8a0737d4
Joanna Huang <joanna.huitzu.huang@gmail.com>
2013-06-12 22:09:41 +1000

    fixed bugs and added one line.

--- time.py
+++ time.py
@@ -2,11 +2,11 @@
 import time
 print time.time()
 
-print "I made some changes on fork."
 print "fork added one line"
 print "collaborator added one line."
 print "collaborator added another line."
 print "upstream added one line."
 print "upstream added another line."
 print "upstream added third line."
-print "upstream added four line."
+print "upstream added fourth line."
+print "upstream added fifth line."
